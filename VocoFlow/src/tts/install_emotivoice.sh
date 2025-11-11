3f406dfa80abc244d60a785bc85f05bcc4d2896c
braving-breeze <cielllll17@gmail.com>
2025-11-11 22:52:09 +0800

    EV-Installer

--- VocoFlow/src/tts/install_emotivoice.sh
+++ VocoFlow/src/tts/install_emotivoice.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Step 1: Create and activate conda environment
-echo "Creating and activating the conda environment..."
-conda create -n EmotiVoice python=3.8 -y
-conda init
-conda activate EmotiVoice
-
-# Step 2: Install git-lfs
+# Step 1: Install git-lfs
 echo "Installing git-lfs..."
 if [ "$(lsb_release -i | grep 'Ubuntu')" ]; then
   sudo apt update
@@ -19,33 +13,33 @@ else
   exit 1
 fi
 
-# Step 3: Clone the EmotiVoice repository
+# Step 2: Clone the EmotiVoice repository
 echo "Cloning the EmotiVoice repository..."
 git lfs install
 git lfs clone https://github.com/netease-youdao/EmotiVoice.git
 
-# Step 4: Install dependencies
+# Step 3: Install dependencies
 echo "Installing required Python packages..."
 pip install torch torchaudio
 pip install numpy numba scipy transformers soundfile yacs g2p_en jieba pypinyin pypinyin_dict
 python -m nltk.downloader "averaged_perceptron_tagger_eng"
 
-# Step 5: Download pre-trained models
+# Step 4: Download pre-trained models
 echo "Downloading pre-trained models..."
 cd EmotiVoice
 git lfs clone https://huggingface.co/WangZeJun/simbert-base-chinese WangZeJun/simbert-base-chinese
 git clone https://www.modelscope.cn/syq163/WangZeJun.git
 git clone https://www.modelscope.cn/syq163/outputs.git
 
-# Step 6: Install Streamlit for UI interface
+# Step 5: Install Streamlit for UI interface
 echo "Installing Streamlit..."
 pip install streamlit
 
-# Step 7: Run the Streamlit UI interface
+# Step 6: Run the Streamlit UI interface
 echo "Running the Streamlit UI interface..."
 streamlit run demo_page.py --server.port 6006 --logger.level debug
 
-# Step 8: Install FastAPI for API service
+# Step 7: Install FastAPI for API service
 echo "Installing FastAPI and other dependencies..."
 pip install fastapi pydub uvicorn[standard] pyrubberband
 
